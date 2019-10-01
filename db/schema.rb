e82501cc558ba0d99f4782e9dd59750e880ab25c
Silva Salazar Omar Sebastian <omar_2332@comunidad.unam.mx>
2019-09-30 23:11:19 -0500

    Tarea 3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191001011910) do
+ActiveRecord::Schema.define(version: 20191001040805) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "post_id"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 20191001011910) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
